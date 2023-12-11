edbd52f7c429b4cad27e4a92abe922f4ad14b355
Fayeemahmad398 <fayeemahmad0@gmail.com>
2023-12-11 21:45:03 +0530

    third

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,45 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "./Component/Navbar"
-import Home from "./Pages/Home"
+import Home from "./Component/Home"
 import "./style/home.css"
+import { IoIosArrowUp } from "react-icons/io";
 
 function App() {
   const [searchTerm, setSearchTerm] = useState("");
+  const [showTopBtn, setShowTopBtn] = useState(false);
+
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 800) {
+        setShowTopBtn(true);
+      } else {
+        setShowTopBtn(false);
+      }
+    });
+  }, []);
+
+
+  const goToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+
+
   return (
     <div>
       <Navbar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       <div className="home-box" >
         <Home searchTerm={searchTerm} />
       </div>
+      <IoIosArrowUp className="moveup" onClick={() => { goToTop() }} style={{ display: `${showTopBtn ? "block" : "none"}` }} />
     </div>
   )
 }
 
 export default App
+
+
